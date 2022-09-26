696fe0acbdb910c8c1f1a45a84d6212bc023959e
ProInCPython <kandry19@mail.ru>
2022-09-26 20:03:29 +0300

    Number 3

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt(),b = in.nextInt(), n = in.nextInt();
-        int rub = a * n + (b * n) / 100;
-        int cop = (b * n) % 100;
-        System.out.print(rub);
-        System.out.print(" ");
-        System.out.print(cop);
+        String minutes;
+        String seconds;
+        int n = in.nextInt();
+        int hours = n / 3600;
+        int minutes1 = (n - (hours * 3600)) / 60;
+        if (minutes1 < 10) {
+            minutes = "0" + minutes1;
+        }
+        else {
+            minutes = "" + minutes1;
+        }
+        int seconds1 = (n - (hours * 3600)) - (minutes1 * 60);
+        if (seconds1 < 10) {
+            seconds = "0" + seconds1;
+        }
+        else {
+            seconds = "" + seconds1;
+        }
+        System.out.println(hours + ":" + minutes + ":" + seconds);
     }
 }
