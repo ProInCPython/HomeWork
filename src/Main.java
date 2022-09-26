dc1f3d743cde51c0241941ee3d19172771b1831d
ProInCPython <kandry19@mail.ru>
2022-09-26 18:02:33 +0300

    My first homework in the Samsung IT School!!! Wow!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int last = n % 10;
+        int first = n / 100;
+        int middle = (n / 10) % 10;
+        System.out.print(last + first + middle);
+        System.out.print(" ");
+        System.out.print(n + (2 - (n % 2)));
     }
 }
