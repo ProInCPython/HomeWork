00cbee476736b1bd0c0bff9205253d4f19a47fb5
ProInCPython <kandry19@mail.ru>
2022-09-27 16:32:25 +0300

    Number 5

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt(),b = in.nextInt(), n = in.nextInt();
-        int rub = a * n + (b * n) / 100;
-        int cop = (b * n) % 100;
-        System.out.print(rub);
-        System.out.print(" ");
-        System.out.print(cop);
+        int a = in.nextInt(), b = in.nextInt(), c = in.nextInt();
+        int counter = 0;
+        if ((a >= 100 & a <= 999) & a % 5 == 0) {
+            counter += 1;
+        }
+        if ((b >= 100 & b <= 999) & b % 5 == 0) {
+            counter += 1;
+        }
+        if ((c >= 100 & c <= 999) & c % 5 == 0) {
+            counter += 1;
+        }
+        if (counter >= 2) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+
     }
 }
