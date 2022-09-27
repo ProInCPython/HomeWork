35a410b982255e283a666b80f23301cab92bab8c
ProInCPython <kandry19@mail.ru>
2022-09-27 16:48:58 +0300

    Small changes

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt(), b = in.nextInt(), c = in.nextInt();
         int counter = 0;
-        if ((a >= 100 & a <= 999) & a % 5 == 0) {
+        if ((a >= 100 && a <= 999) && a % 5 == 0) {
             counter += 1;
         }
-        if ((b >= 100 & b <= 999) & b % 5 == 0) {
+        if ((b >= 100 && b <= 999) && b % 5 == 0) {
             counter += 1;
         }
-        if ((c >= 100 & c <= 999) & c % 5 == 0) {
+        if ((c >= 100 && c <= 999) && c % 5 == 0) {
             counter += 1;
         }
         if (counter >= 2) {
