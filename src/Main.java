361eefa6ac353e158fc084431c0993aa4d8fdefb
ProInCPython <kandry19@mail.ru>
2022-09-27 17:19:17 +0300

    Number 6

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt(), b = in.nextInt(), c = in.nextInt();
-        int counter = 0;
-        if ((a >= 100 & a <= 999) & a % 5 == 0) {
-            counter += 1;
-        }
-        if ((b >= 100 & b <= 999) & b % 5 == 0) {
-            counter += 1;
-        }
-        if ((c >= 100 & c <= 999) & c % 5 == 0) {
-            counter += 1;
-        }
-        if (counter >= 2) {
-            System.out.println("true");
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+        if ((y > 1 - x) && (x < 1) && ((y > 2 * x * x) || (y < 2 * x * x)) && (x > 0)) {
+            System.out.println("YES");
         }
         else {
-            System.out.println("false");
+            System.out.println("NO");
         }
-
     }
 }
