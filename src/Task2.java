c38b0c5e82065e2251f02fb9c4c49586be05f366
ProInCPython <kandry19@mail.ru>
2022-10-04 15:01:28 +0300

    Task 2 in homework 3

--- src/Task2.java
+++ src/Task2.java
@@ -2,34 +2,26 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        int rabbits = 11;
-        int wolves = 2;
-        int eaten_rabbits = 0;
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for (int i = 1; i <= n; ++i) {
-            if (i % 2 != 0) {
-                rabbits *= 3;
-                if (rabbits > 19000000) {
-                    rabbits = 19000000;
-                }
-            } else {
-                if (wolves * 10 > rabbits) {
-                    int wolves_deduction = ((wolves * 10 - rabbits)) / 10 + 1;
-                    wolves -= wolves_deduction;
-                    rabbits -= wolves * 10;
-                    eaten_rabbits += wolves * 10;
-                } else {
-                    rabbits -= wolves * 10;
-                    eaten_rabbits += wolves * 10;
-                }
-            }
-            if (eaten_rabbits >= 70) {
-                wolves += 1;
-                eaten_rabbits -= 70;
-            }
+        while (n % 10 == 0) {
+            n /= 10;
         }
-        System.out.println(rabbits);
-        System.out.println(wolves);
+        if (n < 0) {
+            n -= n * 2;
+            String result = "";
+            while (n != 0) {
+                result += n % 10;
+                n /= 10;
+            }
+            System.out.println("-" + result);
+        } else {
+            String result = "";
+            while (n != 0) {
+                result += n % 10;
+                n /= 10;
+            }
+            System.out.println(result);
+        }
     }
 }
