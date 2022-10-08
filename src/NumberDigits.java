efcd5ff98bc1e17cd440fa6885bd1c392a9ed65d
ProInCPython <kandry19@mail.ru>
2022-10-08 12:06:39 +0300

    Task 1 in homework 4

--- src/NumberDigits.java
+++ src/NumberDigits.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 public class NumberDigits {
     public static int sumOfSeven(int a, int b) {
         int result = 0;
+        if (b > 99) {
+            b = 99;
+        }
         for (int i = a; i <= b; ++i) {
             if (i % 7 == 0) {
                 result += i % 10 + i / 10;
