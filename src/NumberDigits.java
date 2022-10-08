e6efb27dfcbddeee2bdc99045eee7545682b4b21
ProInCPython <kandry19@mail.ru>
2022-10-08 12:01:26 +0300

    Task 1 in homework 4

---
+++ src/NumberDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class NumberDigits {
+    public static int sumOfSeven(int a, int b) {
+        int result = 0;
+        for (int i = a; i <= b; ++i) {
+            if (i % 7 == 0) {
+                result += i % 10 + i / 10;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(sumOfSeven(a, b));
+    }
+}
