746859c462d6f7bf0f5a0e3451c3f1ed841f30cb
Andrey <96537321+ProInCPython@users.noreply.github.com>
2022-10-22 23:43:19 +0300

    Update Task1.java

--- src/Task1.java
+++ src/Task1.java
@@ -4,11 +4,17 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        byte[] arr = new byte[(n / 8) + 1];
+        byte[] arr;
+        if (n <= 7) {
+            arr = new byte[1];
+        } else {
+            arr = new byte[(n / 7) + 1];
+        }
+
         byte mask = (byte) 1000000;
         int numberOfZeros = 0;
         for (int i = 0; i < arr.length; ++i) {
-            for (int j = 0; j < (n <= 7 ? n % 8 : 7); ++j) {
+            for (int j = 0; j < (Math.min(n, 7)); ++j) {
                 if (in.nextBoolean()) {
                     arr[i] = (byte) (arr[i] | mask);
                 }
@@ -21,12 +27,12 @@ public class Task1 {
             }
             n -= 7;
         }
-        System.out.println(Integer.toBinaryString(arr[0]));
-        System.out.println(numberOfZeros);
+        int counter = 1;
         for (byte b : arr) {
-            for (int j = 6 - numberOfZeros; j >= 0; --j) {
+            for (int j = (counter == arr.length ? 6 - numberOfZeros : 6); j >= 0; --j) {
                 System.out.print(((b & (1 << j)) != 0) + " ");
             }
+            counter += 1;
         }
     }
 }
